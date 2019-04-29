17b50f6100fe08e05f7000e4091dc9da59b38844
kbub27 <kalebmerriman27@gmail.com>
2019-04-28 17:41:03 -0700

    took out npm bands in town and used axios have all functions logging data

--- liri.js
+++ liri.js
@@ -1,36 +1,42 @@
 require('dotenv').config();
 
+//REQUIRE ALL APIs AND FILES
 var keys = require('./keys');
+const omdbWrapper = require('omdb-wrapper').default;
+var axios = require('axios');
+var Spotify = require('node-spotify-api');
 
 //QUERY FOR OMDB
-const omdbWrapper = require('omdb-wrapper').default;
-
 const omdb = new omdbWrapper({
     apiKEY: keys.omdbKey
 });
 omdb.search.movies('forrest+gump')
     .then(data => {
         // do what you want with the data
-        console.log(data.Search[0]);
+        console.log('omdb logs data');
+        console.log('\n-----------------\n')
     });
-// QUERY FOR BANDS IN TOWN
-// var bandsintown = require('bandsintown')(keys.bandsID);
 
-// bandsintown.getArtistEventList('skrillex').then(function (events) {
-//     // return array of events
-//     console.log('\n------------\n')
-//     console.log(events)
-// });
+// QUERY FOR BANDS IN TOWN
+var bandsQuery = "https://rest.bandsintown.com/artists/" + 'skrillex' + "/events?app_id=codingbootcamp"
+axios.get(bandsQuery)
+  .then(function (response) {
+    console.log('Venue: ' + response.data[0].venue.name);
+    console.log('Location: ' + response.data[0].venue.country + ',' + response.data[0].venue.city);
+    var moment = require('moment');
+    console.log(moment(response.data[0].datetime).format('MM/DD/YYYY'));
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 
 //QUERY FOR SPOTIFY
-var Spotify = require('node-spotify-api');
-
 var spotify = new Spotify(keys.spotify);
 
 spotify.search({ type: 'track', query: 'All the Small Things' }, function (err, data) {
     if (err) {
         return console.log('Error occurred: ' + err);
     }
-
-    console.log(data.tracks.items[0]);
+    console.log('\n---------------\n')
+    console.log('spotify logs data');
 });
